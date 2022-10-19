49801a127b1e680c3306d01098de047c82a78a51
modbots <77435462+modbots@users.noreply.github.com>
2022-10-19 21:07:34 +0630

    Update proxyfix.py

--- proxyfix.py
+++ proxyfix.py
@@ -1,9 +1,45 @@
+import random
 import requests
 import json
 def proxyfix(a):
     b = a
-    url = "https://raw.githubusercontent.com/UptimerBot/proxy-list/main/proxies/http.txt"#"https://www.proxyscan.io/download?type=http"
-    proxis = requests.get(url).text
+    url = ["https://raw.githubusercontent.com/UptimerBot/proxy-list/main/proxies/http.txt",#"https://www.proxyscan.io/download?type=http"
+    "http://proxysearcher.sourceforge.net/Proxy%20List.php?type=http",
+    "http://worm.rip/http.txt",
+    "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http",
+    "https://openproxy.space/list/http",
+    "https://openproxylist.xyz/http.txt",
+    "https://proxyspace.pro/http.txt",
+    "https://proxyspace.pro/https.txt",
+    "https://raw.githubusercontent.com/almroot/proxylist/master/list.txt",
+    "https://raw.githubusercontent.com/aslisk/proxyhttps/main/https.txt",
+    "https://raw.githubusercontent.com/B4RC0DE-TM/proxy-list/main/HTTP.txt",
+    "https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt",
+    "https://raw.githubusercontent.com/hendrikbgr/Free-Proxy-Repo/master/proxy_list.txt",
+    "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-http.txt",
+    "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-https.txt",
+    "https://raw.githubusercontent.com/mertguvencli/http-proxy-list/main/proxy-list/data.txt",
+    "https://raw.githubusercontent.com/mmpx12/proxy-list/master/http.txt",
+    "https://raw.githubusercontent.com/mmpx12/proxy-list/master/https.txt",
+    "https://raw.githubusercontent.com/proxy4parsing/proxy-list/main/http.txt",
+    "https://raw.githubusercontent.com/roosterkid/openproxylist/main/HTTPS_RAW.txt",
+    "https://raw.githubusercontent.com/RX4096/proxy-list/main/online/http.txt",
+    "https://raw.githubusercontent.com/RX4096/proxy-list/main/online/https.txt",
+    "https://raw.githubusercontent.com/saisuiu/uiu/main/free.txt",
+    "https://raw.githubusercontent.com/saschazesiger/Free-Proxies/master/proxies/http.txt",
+    "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/http.txt",
+    "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/https.txt",
+    "https://raw.githubusercontent.com/sunny9577/proxy-scraper/master/proxies.txt",
+    "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
+    "https://rootjazz.com/proxies/proxies.txt",
+    "https://sheesh.rip/http.txt",
+    "https://spys.me/proxy.txt",
+    "https://www.freeproxychecker.com/result/http_proxies.txt",
+    "https://www.proxy-list.download/api/v1/get?type=http",
+    "https://www.proxy-list.download/api/v1/get?type=https",
+    "https://www.proxyscan.io/download?type=http"]
+    purl =  random.choice(url)
+    proxis = requests.get(purl).text
     proxis = proxis.splitlines()
     for proxy in proxis:
         print(f'checking {proxy}...')
